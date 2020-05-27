2acae411a62676b9c7f366ccf42cedbf5be6c179
Ronit Thakur <cjhacker0@gmail.com>
2020-05-27 08:03:21 +0530

    Fix bugs

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ io.on('connection',socket =>{
         delete users[socket.id];
     });
 });
+
+var fs = require('fs'),
+http = require('http');
+
+http.createServer(function (req, res) {
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(8080);
